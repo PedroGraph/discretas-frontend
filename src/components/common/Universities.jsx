7939ae211dc440db3e4dab38f904ac40cbf6b179
Pmedinam <p.medina@sell2rent.com>
2023-08-17 00:45:29 -0500

    Eliminating Carrusel from main page

--- src/components/common/Universities.jsx
+++ src/components/common/Universities.jsx
@@ -51,7 +51,7 @@ export default function University() {
         
         <div>
           <MainLenceria/>
-          <MostSold/>
+          {/* <MostSold/> */}
           <Footer/>
         </div>
     
