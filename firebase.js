3d9929c16aacbf0a6788aa1bf0a594580a5056ca
Pmedinam <p.medina@sell2rent.com>
2023-09-11 20:15:48 -0500

    Firebase files

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "AIzaSyDXTHZV8NLfPAt-ljO3Cl_-a9RiJ1gt7iY",
+  authDomain: "discreta-seduccion.firebaseapp.com",
+  projectId: "discreta-seduccion",
+  storageBucket: "discreta-seduccion.appspot.com",
+  messagingSenderId: "54422796964",
+  appId: "1:54422796964:web:839aa86873821aac91c431",
+  measurementId: "G-TFQYPBTQLQ"
+});
+
+export default firebaseConfig;
